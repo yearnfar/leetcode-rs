22f88f61ea7fde07f501a152ec8a4f8c6de3e2e1
yearnfar <yearnfar@gmail.com>
2020-12-14 22:08:46 +0800

    feat: 7. 整数反转

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,2 +1,3 @@
 pub mod solution_1;
 pub mod solution_217;
+pub mod solution_7;
---
+++ src/easy/solution_7.rs
@@ -0,0 +1,38 @@
+#![allow(dead_code)]
+use std::i32;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn reverse(x: i32) -> i32 {
+        let mut x: i64 = x as i64;
+        let mut y: i64 = 0;
+        let max = std::i32::MAX as i64;
+        let min = std::i32::MIN as i64;
+
+        while x != 0 {
+            y = (y * 10 + x % 10);
+            if y > max || y < min {
+                return 0;
+            }
+
+            x /= 10;
+        }
+
+        let y = y as i32;
+        y
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run1() {
+        assert_eq!(Solution::reverse(123), 321);
+        assert_eq!(Solution::reverse(-123), -321);
+        assert_eq!(Solution::reverse(120), 21);
+        assert_eq!(Solution::reverse(1534236469), 0);
+    }
+}
